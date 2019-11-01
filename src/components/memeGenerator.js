34941f5630540dd94a34e4ecbc22b16a0dbeb2a7
YuriySuro <suro1989@ukr.net>
2019-11-01 13:24:23 +0200

    add form to Meme Generator

--- src/components/memeGenerator.js
+++ src/components/memeGenerator.js
@@ -9,6 +9,8 @@ class MemeGenerator extends React.PureComponent {
             randomImg: 'http://i.imgflip.com/1bij.jpg',
             allMemesImgs: []
         };
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
@@ -21,11 +23,17 @@ class MemeGenerator extends React.PureComponent {
         });
     }
 
+    handleChange(event) {
+        console.log("Working!");
+    }
+
     render() {
         return (
             <form>
                 <div className="form-group">
-                        <button className="btn btn-success">Generate</button>
+                    <input type="text" id="topText" placeholder="Top Text" value={this.state.topText} onChange={this.handleChange} />
+                    <input type="text" id="bottomText" placeholder="Bottom Text" value={this.state.bottomText} onChange={this.handleChange} />
+                    <button className="btn btn-success">Generate</button>
                 </div>
             </form>
         );
